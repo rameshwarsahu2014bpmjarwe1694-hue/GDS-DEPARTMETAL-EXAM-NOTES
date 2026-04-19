ebdda224a12523e4f5a2fb2864bfe2d7793f23cf
https://github.com/rameshwarsahu/GDS-DEPARTMETAL-EXAM-NOTES.git <rameshwarsahu2014bpmjarwe1694@gmail.com>
2026-04-19 22:18:18 +0530

    Update firebase.js
    Signed-off-by: https://github.com/rameshwarsahu/GDS-DEPARTMETAL-EXAM-NOTES.git <rameshwarsahu2014bpmjarwe1694@gmail.com>

--- firebase.js
+++ firebase.js
@@ -1,24 +1,33 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-app.js";
+
 import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signOut
+getAuth,
+createUserWithEmailAndPassword,
+signInWithEmailAndPassword,
+signOut
 } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-auth.js";
 
+import {
+getFirestore
+} from "https://www.gstatic.com/firebasejs/12.12.0/firebase-firestore.js";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDjzmo0A4KhOkrqFQdOyCEek6bsPF-UVPQ",
-  authDomain: "gds-departmental-exam-studyhub.firebaseapp.com",
-  projectId: "gds-departmental-exam-studyhub",
-  storageBucket: "gds-departmental-exam-studyhub.firebasestorage.app",
-  messagingSenderId: "759916756080",
-  appId: "1:759916756080:web:7f2c0e66458bd007e888b5"
+apiKey: "AIzaSyDjzmo0A4KhOkrqFQdOyCEek6bsPF-UVPQ",
+authDomain: "gds-departmental-exam-studyhub.firebaseapp.com",
+projectId: "gds-departmental-exam-studyhub",
+storageBucket: "gds-departmental-exam-studyhub.firebasestorage.app",
+messagingSenderId: "759916756080",
+appId: "1:759916756080:web:7f2c0e66458bd007e888b5"
 };
 
 const app = initializeApp(firebaseConfig);
+
 const auth = getAuth(app);
+const db = getFirestore(app);
 
 window.auth = auth;
+window.db = db;
+
 window.createUserWithEmailAndPassword = createUserWithEmailAndPassword;
 window.signInWithEmailAndPassword = signInWithEmailAndPassword;
 window.signOut = signOut;
