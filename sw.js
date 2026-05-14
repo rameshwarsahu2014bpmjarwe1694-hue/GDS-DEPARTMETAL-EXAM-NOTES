6d4c4d73fee090e992d218a71ac34ae500b0146b
https://github.com/RS/Online/GDS/Departmental/Exam/Study/Hub. git <rameshwarsahu2014bpmjarwe1694@gmail.com>
2026-05-14 13:19:15 +0530

    Update service worker cache version and logic
    Signed-off-by: https://github.com/RS/Online/GDS/Departmental/Exam/Study/Hub. git <rameshwarsahu2014bpmjarwe1694@gmail.com>

--- sw.js
+++ sw.js
@@ -1,30 +1,68 @@
-const CACHE_NAME = "gds-cache-v1";
+const CACHE_NAME = "gds-cache-v2";
 
 const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png",
+  "/RSH.png"
 ];
 
+// INSTALL
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(urlsToCache);
     })
   );
+
   self.skipWaiting();
 });
 
+// ACTIVATE
 self.addEventListener("activate", (event) => {
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
     })
   );
+
+  self.clients.claim();
+});
+
+// FETCH
+self.addEventListener("fetch", (event) => {
+
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+
+      if (response) {
+        return response;
+      }
+
+      return fetch(event.request)
+        .then((networkResponse) => {
+
+          return caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+          });
+
+        })
+        .catch(() => {
+          return caches.match("/index.html");
+        });
+
+    })
+  );
+
 });
